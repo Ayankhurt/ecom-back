540e81ac2e76b71e9fd59a66c551bfddcc2a04d8
Ayan Khurt <ayankhurt@gmail.com>
2025-06-20 02:47:09 +0500

    new

--- server.mjs
+++ server.mjs
@@ -1,10 +1,14 @@
+import 'dotenv/config';
 import express from "express";
 import cors from "cors";
 import db from "./db.mjs";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { customAlphabet } from 'nanoid'
 
+const SECRET = process.env.SECRET_TOKEN;
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 /// new ///
 app.use(express.json());
@@ -19,12 +23,14 @@ app.get("/", async (req, res) => {
     res.status(500).send({ message: "Error fetching data", error: error.message });
   }
 });
+
 app.post("/sign-up", async (req, res) => {
   let reqbody = req.body;
   if (!reqbody.firstName || !reqbody.lastName || !reqbody.email || !reqbody.password) {
     res.status(400).send({ message: "All fields are required" });
     return;
   }
+
   reqbody.email = reqbody.email.toLowerCase();
   let query = `SELECT * FROM users WHERE email = $1`;
   let values = [reqbody.email];
@@ -45,22 +51,61 @@ app.post("/sign-up", async (req, res) => {
     res.status(500).send({ message: "Error creating user", error: error.message });
   };
 })
-app.post("/login", async (req, res) => {
-  let reqbody = req.body;
-  if (!reqbody.email || !reqbody.password) {
-    res.status(400).send({ message: "All fields are required" });
+app.post('/login', async (req, res) => {
+  let reqBody = req.body;
+  if (!reqBody.email || !reqBody.password) {
+    res.status(400).send({ message: "Required Parameter Missing" })
     return;
   }
+  reqBody.email = reqBody.email.toLowerCase();
   let query = `SELECT * FROM users WHERE email = $1`;
-  let values = [reqbody.email];
+  let values = [reqBody.email];
 
   try {
     let result = await db.query(query, values);
-    if (!result.rows?.length) {
-      res.status(400).send({ message: "user does not exist with this email" });
+    if (!result.rows.length) {
+      res.status(400).send({ message: "User Doesn't exist with this Email" });
+      return;
     }
+
+    let isMatched = await bcrypt.compare(reqBody.password, result.rows[0].password); // true
+
+    if (!isMatched) {
+      res.status(401).send({ message: "Password did not Matched" });
+      return;
+    }
+
+    let token = jwt.sign({
+      id: result.rows[0].user_id,
+      firstName: result.rows[0].first_name,
+      last_name: result.rows[0].last_name,
+      email: result.rows[0].email,
+      user_role: result.rows[0].user_role,
+      iat: Date.now() / 1000,
+      exp: (Date.now() / 1000) + (1000 * 60 * 60 * 24)
+    }, SECRET);
+    res.cookie('Token', token, {
+      maxAge: 86400000,
+      httpOnly: true,
+      secure: true
+    });
+    res.status(200)
+    res.send({
+      message: "User Logged in", token, user: {
+        user_id: result.rows[0].user_id,
+        first_name: result.rows[0].first_name,
+        last_name: result.rows[0].last_name,
+        email: result.rows[0].email,
+        phone: result.rows[0].phone,
+        user_role: result.rows[0].user_role,
+        profile: result.rows[0].profile,
+      }
+    })
+
+
   } catch (error) {
-    res.status(500).send({ message: "Error logging in", error: error.message });
+    console.log("Error", error)
+    res.status(500).send({ message: "Internal Server Error" })
   }
 })
 
